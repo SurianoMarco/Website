ff16dfc97f5f09d00759d06b7cc894bdf72f4241
SurianoMarco <63708492+SurianoMarco@users.noreply.github.com>
2020-06-05 17:02:46 +0200

    Another update

--- app.js
+++ app.js
@@ -1,29 +1,61 @@
 
 
-  
-  setTimeout(function start (){
-  
-  $('.bar').each(function(i){  
-    var $bar = $(this);
-    $(this).append('<span class="count"></span>')
-    setTimeout(function(){
-      $bar.css('width', $bar.attr('data-percent'));      
-    }, i*100);
-  });
+(function($){
+  $(document).ready(function(){
+    var $els = $('.Graph'); // Note this must be moved to within event handler if dynamically adding elements - I've placed it for performance reasons 
+    var $window = $(window);
+    
+    $window.on('scroll', function(){
+      $els.each(function(){ // Iterate over all skillbars
+        var $this = $(this); 
+        if($window.scrollTop() > $this.offset().top - $window.height()){ // Check if it's in viewport
+        
+      
+          $('.bar').each(function(i){  
+            var $bar = $(this);
+            $(this).append('<span class="count"></span>')
+            setTimeout(function(){
+              $bar.css('width', $bar.attr('data-percent'));      
+            }, i*100);
+          });
+        
+        $('.count').each(function () {
+            $(this).prop('Counter',0).animate({
+                Counter: $(this).parent('.bar').attr('data-percent')
+            }, {
+                duration: 2000,
+                easing: 'swing',
+                step: function (now) {
+                    $(this).text(Math.ceil(now) +'%');
+                }
+            });
+        });
 
-$('.count').each(function () {
-    $(this).prop('Counter',0).animate({
-        Counter: $(this).parent('.bar').attr('data-percent')
-    }, {
-        duration: 2000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now) +'%');
+
+
+
+
         }
+      });
+    
     });
-});
+  });
+}(jQuery));
 
-}, 500)
+
+
+ 
+
+
+
+
+
+    
+
+
+  
+ 
+
 
 
 
