82d6b1a80fc89181d0d2eec78e626c51527191e9
SurianoMarco <63708492+SurianoMarco@users.noreply.github.com>
2020-07-02 17:39:31 +0200

    Form working

--- app.js
+++ app.js
@@ -1,4 +1,40 @@
+$(document).ready(function(){
+  $('.submit').click(function(event){
+    console.log('Clicked this button')
 
+
+    var name = $('.subject').val()
+    var email = $('.email').val()
+    var message = $('.message').val()
+    var statusElm = $('.status')
+    statusElm.empty()
+
+if (email.length > 5 && email.includes('@')&& email.includes('.')) {
+  statusElm.append('<div>Email is valid</div>')
+} else {
+  event.preventDefault()
+  statusElm.append('<div>Email is not valid</div>')
+}
+
+if (name.length > 2){
+  statusElm.append('<div>Name is valid</div>')
+
+} else {
+  event.preventDefault()
+  statusElm.append('<div>Name is not valid</div>')
+}
+
+if (message.length > 20){
+  statusElm.append('<div>Message is valid</div>')
+} else {
+  event.preventDefault()
+  statusElm.append('<div>Message is not valid</div>')
+}
+
+
+
+  })
+})
 
 
 
